e92716ec42d82cb14b080fdcdd5a888e17936ac0
gholdman1 <gholdman@protonmail.com>
2019-04-26 16:07:02 -0500

    Change FOM to be the squared magnitude, and not HALF of the squared magnitude

--- lumopt/figures_of_merit/magnitude.py
+++ lumopt/figures_of_merit/magnitude.py
@@ -39,13 +39,14 @@ class PointElectric(object):
 
     def add_to_sim(self, sim):
         """
-        Not used in this FOM.
+        Probably not used in this FOM, but I'm not sure what its purpose
+        is in ModeMatch.
         """
         pass
 
     def get_fom(self, sim):
         """
-        FOM is HALF of the squared magnitude of the E-field at monitor 'fom', a la
+        FOM is the squared magnitude of the E-field at monitor 'fom', a la
         Section 5.1 of Ref 1
 
         References
@@ -62,7 +63,7 @@ class PointElectric(object):
 
         self.forward_E = E_field_object.E.squeeze()
         self.phase_prefactors = np.linalg.norm(self.forward_E)
-        fom = 0.5 * np.linalg.norm(self.forward_E)**2
+        fom = np.linalg.norm(self.forward_E)**2
         return fom
 
     def get_adjoint_field_scaling(self, sim):
