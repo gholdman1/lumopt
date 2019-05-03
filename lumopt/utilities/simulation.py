0cc079397e6108db3317c4209c3d5e21e1ad237c
gholdman1 <gholdman@protonmail.com>
2019-05-02 19:41:50 -0500

    Add configuration for workqueue as a parameter of the Simulation object. Intent is make the run function send a job to cluster

--- lumopt/utilities/simulation.py
+++ lumopt/utilities/simulation.py
@@ -11,16 +11,26 @@ class Simulation(object):
         ----------
         :param workingDir:    working directory for the CAD session.
         :param hide_fdtd_cad: if true, runs the FDTD CAD in the background.
-    """
+        :param wq_config:     dict, if using WorkQueue, provides the configuration.
+                                e.g. wq_config = dict(  {'scheduler':'slurm'},
+                                                        {''},
+                                                        {},
+                                                        )                        
+        """
 
-    def __init__(self, workingDir, hide_fdtd_cad):
+    def __init__(self, workingDir, hide_fdtd_cad, wq_config=None):
         """ Launches FDTD CAD and stores a handle. """
         self.fdtd = lumapi.FDTD(hide = hide_fdtd_cad)
         self.fdtd.cd(workingDir)
 
+        if wq_config:
+            self.wq_config=wq_config
+            
+
     def run(self, name, iter):
         """ Saves simulation file and runs the simulation. """
-        self.fdtd.save('{}_{}'.format(name,iter))
+        filename='{}_{}'.format(name,iter)
+        self.fdtd.save(filename)
         self.fdtd.run()
 
     def __del__(self):
